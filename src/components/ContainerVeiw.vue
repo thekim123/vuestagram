9a1594fb3ca9ded6e57e69b416da63411d26541b
thekim123 <tacdo12@naver.com>
2023-04-14 19:05:03 +0900

    1강 듣다가 커밋

---
+++ src/components/ContainerVeiw.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <PostView :p="postData[0]"/>
+    <PostView :p="postData[1]"/>
+    <PostView :p="postData[2]"/>
+  </div>
+</template>
+
+<script>
+import PostView from './PostView.vue';
+
+export default {
+    name: "ContainerView",
+    components:{
+        PostView,
+    },
+    data(){
+        return{
+            p: postData,
+        }
+    },
+    props:{
+        postData: Array,
+    }
+
+};
+</script>
+
+<style>
+</style>
